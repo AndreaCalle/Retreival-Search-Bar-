8d8dbe9074c736c4b07851378f53fbcd4a0a2646
AndreaCalle <43764730+AndreaCalle@users.noreply.github.com>
2019-06-12 15:17:02 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,25 +8,33 @@ HAS NOT BEEN COMPILE !!!!
 */
 public class Main {
  
-         /*		
-        Scanner in = new Scanner(System.in);
-		System.out.println("Welcome to CPS842 Assignment 2");
-		System.out.println("Choose File(query.text or cacm.all) :");
-		String fileName= in.nextLine();
-		System.out.println("Stemming(On/Off): ");
-		String stem= in.nextLine();
-		makeHashMaps(fileName.trim().toLowerCase(),stem.trim().toLowerCase());
-		System.out.println("Stop Word Removal(On/Off):");
-		String removeWord = in.nextLine();
-		File commonWord = new File("common_words"); 
-        BufferedReader ch = new BufferedReader(new FileReader(commonWord));
-        removeWord = removeWord.trim().toLowerCase(); 
-        
-        ASK FOR CACM.ALL , WITH OR WITHOUT WORD REOVE 
-        */ 
- 
     public static void main(String[] args) throws IOException {
         Invert invert  = new Invert() ; 
+
+        Scanner in = new Scanner(System.in);
+		System.out.println("Welcome to CPS842 Assignment 2"+"\n"+"Choose File(query.text or cacm.all) :");
+        String fileName= in.nextLine();
+        
+		System.out.println("Stemming(On/Off): ");
+        String stem= in.nextLine();
+        
+		//makeHashMaps(fileName.trim().toLowerCase(),stem.trim().toLowerCase());
+		System.out.println("Stop Word Removal(On/Off):");
+        String removeWord = in.nextLine();
+        /*
+		File commonWord = new File("common_words"); 
+        BufferedReader ch = new BufferedReader(new FileReader(commonWord));
+        removeWord = removeWord.trim().toLowerCase();
+        */
+        call(invert,stem,removeWord,fileName);
+
+    }
+/*
+Call methond will call the invert class and run the 
+*/
+    public static void call(Invert invert,String stem,String removeWord,String fileName)
+    {
+		//makeHashMaps(fileName.trim().toLowerCase(),stem.trim().toLowerCase());
     }
 
 
