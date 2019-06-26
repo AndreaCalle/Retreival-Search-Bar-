85ba9f8f1277f9c46b55a4934bbf2edfd761cced
AndreaCalle <43764730+AndreaCalle@users.noreply.github.com>
2019-06-26 15:00:29 -0400

    Add files via upload

--- SearchBar.java
+++ SearchBar.java
@@ -4,6 +4,8 @@ this is the GUI Search bar
 */
 
 import javax.swing.*;
+import java.awt.*;
+
 class SearchBar{
       public static void main(String args[]){
            JFrame frame = new JFrame("My First GUI");
@@ -11,6 +13,20 @@ class SearchBar{
            frame.setSize(300,300);
           JButton button1 = new JButton("Button 1");
           JButton button2 = new JButton("Button 2");
+
+
+          JPanel panel = new JPanel(); // the panel is not visible in output
+          JLabel label = new JLabel("Enter Text");
+          JTextField tf = new JTextField(10); // accepts upto 10 characters
+          JButton send = new JButton("Send");
+          JButton reset = new JButton("Reset");
+          panel.add(label); // Components Added using Flow Layout
+          panel.add(label); // Components Added using Flow Layout
+          panel.add(tf);
+          panel.add(send);
+          panel.add(reset);
+
+          frame.getContentPane().add(BorderLayout.SOUTH, panel);
           frame.getContentPane().add(button1);
           frame.getContentPane().add(button2);
           frame.setVisible(true);
