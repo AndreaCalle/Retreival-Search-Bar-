f88ac012c71bed71510c687b84e4b3bb0218f6dc
AndreaCalle <43764730+AndreaCalle@users.noreply.github.com>
2019-08-06 14:18:53 -0400

    Add files via upload

--- SearchBar.java
+++ SearchBar.java
@@ -6,34 +6,36 @@ must be completed
 1. need a section where word research result show
 2. buttons need to connect the fumctions
 3. will have a loading bar 
-4. need title 
-5. once connect need to mak the method none static 
-6. need a radio buttom for 
+4. need a radio buttom for 
+5.create a text box for input 
 */
 /*
-will finish tomorrow (Thursday)
 not compile yet 
-try to use pagerank 
+trying to use pagerank 
 */
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.* ; 
+
 
 class SearchBar{
       public static void main(String args[]){
-          ProjectMain test = new ProjectMain() ; 
-          test.
+          Main test = new Main() ; 
+          //test.
           JFrame frame = new JFrame("Search query");
           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
           frame.setSize(300,300);
           JButton button1 = new JButton("search");
-          JButton button2 = new JButton("Button 2");
-
+          //JButton button2 = new JButton("Button 2");
+          JRadioButton check = new JRadioButton();
 
           JPanel panel = new JPanel(); // the panel is not visible in output
           JLabel label = new JLabel("Enter Text");
           JTextField tf = new JTextField(10); // accepts upto 10 characters
           JButton send = new JButton("Send");
+     //textField.addActionListener(this); 
+
           JButton reset = new JButton("Reset");
           panel.add(label); // Components Added using Flow Layout
           panel.add(label); // Components Added using Flow Layout
@@ -41,9 +43,26 @@ class SearchBar{
           panel.add(send);
           panel.add(reset);
 
+          send.addActionListener(new ActionListener(){
+               public void actionPerformed(ActionEvent evt) {
+                    String text = tf.getText();
+                    test.sendText(text); 
+                    //System.out.println(text);
+
+                } 
+          }  );
+
           frame.getContentPane().add(BorderLayout.SOUTH, panel);
           frame.getContentPane().add(button1);
           frame.getContentPane().add(button2);
           frame.setVisible(true);
      }
+/*
+     public void actionPerformed(ActionEvent evt) {
+          String text = tf.getText();
+          System.out.println(text);
+         // textArea.append(text + newline);
+          //tf.selectAll();
+      }
+      */
 }
